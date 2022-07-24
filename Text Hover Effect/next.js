038ab3dd85d45c99084601b0cabd0ffbe1197436
Oleksandr Syzoniuk <syzoniuk@ut.ee>
2022-07-24 22:43:04 +0300

    Completed 1st T H E tutorial

---
+++ Text Hover Effect/next.js
@@ -0,0 +1,19 @@
+const grade = 87;
+
+switch (true){
+	case grade >= 90:
+		console.log("Grade is A");
+		break;
+	case grade >=80:
+		console.log("Grade is B");
+		break;
+	case grade >=70:
+		console.log("Grade is C");
+		break;
+	case grade >=60:
+		console.log("Grade is D");
+		break;
+	default:
+		console.log("Grade is F")			
+				
+	}
